e654eb6ec17959510591e193c85fb515ee369eff
Spiceapps <evilspice@gmail.com>
2022-06-16 14:37:27 +0300

    added network, subnet and one VM

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 output "resource_group_name" {
     value = azurerm_resource_group.rg.name
 }
+output "public_ip_address" {
+  value = azurerm_linux_virtual_machine.myterraformvm.public_ip_address
+}
+output "vm_password" {
+    value = azurerm_linux_virtual_machine.myterraformvm.admin_password
+    sensitive = true
+}
